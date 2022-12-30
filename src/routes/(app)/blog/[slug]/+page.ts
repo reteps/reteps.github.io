b4a5868ec94276f73eab1a1c08bcc392dc0b5a84
Pete Stenger <pete@stenger.io>
2022-12-29 23:03:14 -0500

    restructure for time machine

--- src/routes/(app)/blog/[slug]/+page.ts
+++ src/routes/(app)/blog/[slug]/+page.ts
@@ -1,5 +1,5 @@
 export async function load({ params } : { params: any }){
-  const post = await import(`../../../../blog/${params.slug}/index.md`)
+  const post = await import(`../../../../../blog/${params.slug}/index.md`)
 
   return {
     meta : { ...post.metadata },
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,2 @@
-<script>
-  import '$lib/styles/global.css'
-  import '$lib/styles/prism-ghcolors.css'
-  import Header from '$lib/components/Header.svelte'
-  import Footer from '$lib/components/Footer.svelte'
-  import { darkTheme } from '$lib/stores'
-
-  $: dark = $darkTheme // bind to dark class
-  $: bg_color = dark ? 'bg-black' : 'bg-white'
-</script>
-
-<div class:dark class={bg_color} >
-  <Header />
-  
-  <main class="flex flex-col items-center">
-    <div class="container">
-      <slot />
-    </div>
-  </main>
-  
-  <Footer />
-</div>
+<!-- Default to no layout -->
+<slot />
