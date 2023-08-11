041ab7c128abe555425a68ea1eedc81aed3397ce
Pete Stenger <pete@stenger.io>
2023-08-11 18:47:08 -0400

    Update with August 2023 resume

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -26,7 +26,7 @@ header {
         <a href="/blog" aria-label="Blog">Blog</a>
       </li>
       <li class={linkClass}>
-        <a href="https://drive.google.com/file/d/1BilrPM7IPKb680bVsoH-QtpXxuXbUKqu/view" rel="noreferrer" aria-label="Resume">Resume</a>
+        <a href="https://drive.google.com/file/d/1umKtu_Ah_T9ffyfSxc77djtx9-lv4drx/view" rel="noreferrer" aria-label="Resume">Resume</a>
       </li>
       <li class={linkClass}>
         <button on:click={() => $darkTheme = !$darkTheme} aria-label="Toggle">
